8ad3f046afb43c7fd58110cb8d56ba19d625ac3d
XSZ <1273343014@qq.com>
2023-11-22 00:07:27 +0800

    2023/11/21 使用引号传递字符串,使用js变量函数调用,方法调用,使用js对象

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 //项目的根组件
+let count = 100;
 
 function App() {
-  return (
-    <div className="App">
-     this is App
-    </div>
-  );
+
+    return (
+        <div className="App">
+            this is App
+            {/*  使用引号传递字符串  */}
+            {'this   is   message'}
+            {/*  使用js变量  */}
+            {count}
+            {/*函数调用*/}
+            {getName()}
+            {/*方法调用*/}
+            {new Date().getDate()}
+            {/*  使用js对象  */}
+            <div style={{color: 'red'}}>this is jia mengying</div>
+        </div>
+    );
+
+    function getName() {
+        return 'Allen'
+    }
 }
 
 export default App;
